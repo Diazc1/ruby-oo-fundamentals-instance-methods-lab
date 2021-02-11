6b7a8deb712cb182560b90a039a4d75845348f5e
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-10 21:44:58 -0800

    commit tree 3e46c25e96726aef952ae3a687ec7e8e7960e6ed

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog       #add a class definition for a Dog class
+    def bark
+        puts "Woof!"    #def bark/ puts woof/ end --> is #instance method definition
+    end
+    def sit
+        puts "The Dog is sitting"    #add an instance method #sit to your dog class that will puts "The Dog is sitting"
+    end
+end
+
+
+#define bark in Dog- add an instance method #bark to your Dog class that will puts "Woof!"
+#instance methods are methods defined within the object's class because they are methods that belong to any instance of the class
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person              #add a class definition for a Person class
+    def talk
+        puts "Hello World!"      #add an instance method #talk to your person class that will puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
